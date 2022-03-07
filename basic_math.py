ab6113f6c7d28f27fddf82ec867cd637af8b1537
Noah McCaskill <noah.mccaskill@mths.ca>
2022-03-07 15:25:22 +0000

    reformatted

--- basic_math.py
+++ basic_math.py
@@ -11,10 +11,9 @@ def main():
     print("If a rectangle has the dimensions: ")
     print("5cm x 3cm")
     print("")
-    print("Area is {}cm⌃2.".format(5*3))
-    print("Perimeter is {}cm.".format(2 *(5+3)))
+    print("Area is {}cm⌃2.".format(5 * 3))
+    print("Perimeter is {}cm.".format(2 * (5 + 3)))
 
 
 if __name__ == "__main__":
     main()
-    
