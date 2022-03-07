4ee123c8ff00d7b68f5122bfe048438f39a420e9
Noah McCaskill <noah.mccaskill@mths.ca>
2022-03-07 01:45:27 +0000

    added basic_math

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Noah McCaskill
+# Created on: March 2022
+# This program calculates the area and perimeter of a rectangle
+#    with dimensions 5cm x 3cm
+
+
+def main():
+    # this function calculates the area and perimeter
+    print("If a rectangle has the dimensions: ")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm⌃2.".format(5*3))
+    print("Perimeter is {}cm.".format(2 *(5+3)))
+
+
+if __name__ == "__main__":
+    main()
+    
